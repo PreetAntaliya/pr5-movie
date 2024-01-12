446541a293578a86d0b080e6df5b824d79f0103f
preet <preetantaliya@gmail.com>
2024-01-12 19:53:01 +0530

    css

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const app = express()
 const path = require('path');
 const db = require('./config/db')
 
+
 app.set('view engine','ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/', require('./routes/index'))
 app.use('/uploads',express.static(path.join(__dirname,'uploads')))
+app.use('/public',express.static(path.join(__dirname,'public')))
 
 
 app.listen(port, (err) => {
